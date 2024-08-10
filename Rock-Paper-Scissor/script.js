f3e248f5ac0b04ece5a1b0777d168b530b9c879d
Abdul Wahab Khan Arib <haribkhan0625@gmail.com>
2024-08-10 01:34:42 -0700

    javascript projects

--- Rock-Paper-Scissor/script.js
+++ Rock-Paper-Scissor/script.js
@@ -7,58 +7,71 @@ const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const computerScorePara = document.querySelector("#computer-score");
 
+const resetGameBtn = document.querySelector(".reset-btn");
 const genComputerChoice = () => {
-    const options = ["rock", "paper", "scissors"];
-    const randIdx = Math.floor(Math.random() * 3); /*randIdx is being used to access the elements in the array as we know that it cant be done with strings in an array so we will create an randIdx using Math function which will assign the value to the array elements*/
-    return options[randIdx];
-}
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
 
 const drawGame = () => {
-    msg.innerText = "Game was 'Draw!'";
-    msg.style.backgroundColor = "#081b31";
+  msg.innerText = "Game was 'Draw!'";
+  msg.style.backgroundColor = "#081b31";
+};
 
-}
-
-showWinner = (userWin, userChoice, compChoice) => {
-    if(userWin){
-        userScore++;
-        userScorePara.innerText = userScore;
-        msg.innerText = `You Win! Your '${userChoice}' beats computer's '${compChoice}'`;
-        msg.style.backgroundColor = "green";
-    } else{
-        computerScore++;
-        computerScorePara.innerText = computerScore;
-        msg.innerText = `You Lose! Computers '${compChoice}' beats your '${userChoice}'`;
-        msg.style.backgroundColor = "red";
-
-    }
-}
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You Win! Your '${userChoice}' beats computer's '${compChoice}'`;
+    msg.style.backgroundColor = "green";
+  } else {
+    computerScore++;
+    computerScorePara.innerText = computerScore;
+    msg.innerText = `You Lose! Computer's '${compChoice}' beats your '${userChoice}'`;
+    msg.style.backgroundColor = "red";
+  }
+};
 
 const playGame = (userChoice) => {
-    // generate computer choice
-    const compChoice = genComputerChoice();
+  const compChoice = genComputerChoice();
 
-    if(userChoice === compChoice){
-        drawGame();
-    } else{
-        let userWin = true;
-        if(userChoice === "rock"){
-            //scissors, paper
-            userWin = compChoice === "paper" ? false : true;
-        } else if(userChoice === "paper"){
-            //rock,scissors
-            userWin = compChoice === "scissors" ? false : true;
-        } else if(userChoice === "scissors"){
-            //paper,rock
-            userWin = compChoice === "rock" ? false : true;
-        }
-        showWinner(userWin,userChoice,compChoice);
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else if (userChoice === "scissors") {
+      userWin = compChoice === "rock" ? false : true;
     }
-}
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
-    // console.log("choice was clicked",userChoiceId);
     playGame(userChoice);
   });
 });
+
+const resetGame = () => {
+  userScore = 0; // Reset the user's score
+  computerScore = 0; // Reset the computer's score
+  userScorePara.innerText = userScore; // Display the reset score
+  computerScorePara.innerText = computerScore; // Display the reset score
+  msg.innerText = "Play your move"; // Reset the message text
+  msg.style.backgroundColor = "#081b31"; // Reset the background color
+};
+
+const newGame = () => {
+  resetGameBtn.addEventListener("click", () => {
+    resetGame();
+  });
+};
+
+// Call newGame to set up the event listener
+newGame();
